e4fff340956c76c9a03453f5174e03460c1f8186
Douglas Uretsky <douglasuretsky@gmail.com>
2019-02-09 15:49:48 -0500

    create nested resources for users--the parent--and beers--the child

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
   def home
+    
   end
 
   def login
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  post '/rate' => 'rater#create', :as => 'rate'
   get '/login', to: 'application#home'
   post '/login', to: 'application#login'
   get '/logout' => 'application#logout'
   resources :medals
   resources :sessions
   resources :beers
-  resources :users
+  resources :users do
+    resources :beers, only: [:new, :index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
