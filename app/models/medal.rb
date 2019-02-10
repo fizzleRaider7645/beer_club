4620b2ffaebbf7bc445e38c506f8bfd81c21bed3
Douglas Uretsky <douglasuretsky@gmail.com>
2019-02-10 07:52:26 -0500

    create award_medal method on medal class

--- app/models/medal.rb
+++ app/models/medal.rb
@@ -2,36 +2,10 @@ class Medal < ApplicationRecord
   # include Singleton
   has_and_belongs_to_many :users
 
-  @@medals = {
-    :first => Medal.all[1],
-    :second => Medal.all[2],
-    :third => Medal.all[3]
-  }
+  @@medals = Medal.all
 
-  def self.up_for_a_medal?(user)
-    return @@medals[:first] if first_medal(user)
-    return @@medals[:second] if second_medal(user)
-    return @@medals[:third] if third_medal(user)
+  def self.award_medal(user, medal)
+    user.medals << @@medals[medal] unless user.medals.include?(@@medals[medal])
   end
 
-
-  private
-
-  def not_yet_awarded(user, medal)
-    !user.medals.include(medal)
-  end
-
-  def first_medal(user)
-    true if user.beers.count >= 1 && not_yet_awarded(user, @@medals[:first])
-  end
-
-  def second_medal(user)
-    true if user.beers.map { |beer| beer.country }.uniq.count >= 5 && not_yet_awarded(user,
-      @@medals[:second])
-  end
-
-  def third_medal(user)
-    true if user.beers.map { |beer| beer.country }.uniq.count >= 10 && not_yet_awarded(user,
-      @@medals[:third])
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,19 @@ class User < ApplicationRecord
   has_and_belongs_to_many :medals
 
 
-  def award_first_medal
-    self.medals << Medal.all.first
+  def award_rookie_medal?
+    if self.beers.count >= 1
+      Medal.award_medal(self, 0)
+    else
+      return
+    end
+  end
+
+  def award_well_traveled_medal?
+    if self.beers.map { |beer| beer.country }.uniq.count >= 2
+      Medal.award_medal(self, 1)
+    else
+      return
+    end
   end
 end
