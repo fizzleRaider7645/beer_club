bbdbca6eec62080261012f0dc8d27ef489ea4707
Douglas Uretsky <douglasuretsky@gmail.com>
2019-02-13 13:05:54 -0500

    update new review form to handle nested params

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,22 +6,24 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
-    @review.user = current_user
-    @beer = Beer.find_by(name: params[:review][:beers][:name].titleize)
-    if @beer.present?
-      @review.beer = @beer
-      @review.save
-    else
-      @review.build_beer(params.require(:review).require(:beers).permit(:name,
-        :style, :country, :ABV, :IBU))
-      if @review.beer.name.blank?
-        @review.errors[:name_error] << "Must Enter a Beer Name to Submit"
-        render :new
-      else
-        @review.save
-        redirect_to user_path(current_user)
-      end
-    end
+    # binding.pry
+    # @review.user = current_user
+    # @beer = Beer.find_by(name: params[:review][:beers][:name].titleize)
+    # if @beer.present?
+    #   @review.beer = @beer
+    #   @review.save
+    #   redirect_to user_path(current_user)
+    # else
+    #   @review.build_beer(params.require(:review).require(:beers).permit(:name,
+    #     :style, :country, :ABV, :IBU))
+    #   if @review.beer.name.blank?
+    #     @review.errors[:name_error] << "Must Enter a Beer Name to Submit"
+    #     render :new
+    #   else
+    #     @review.save
+    #     redirect_to user_path(current_user)
+    #   end
+    # end
   end
 
   def show
@@ -34,6 +36,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:title, :date, :text, :rating, :beers)
+    params.require(:review).permit(:title, :date, :text, :rating, beer_attributes: [:name,
+      :style, :country, :ABV, :IBU])
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,13 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :beer
   validates :rating, :inclusion => { :in => 0..5 }
+  accepts_nested_attributes_for :beer
+
+
+  def beers=(beer_attributes)
+    beer = Beer.find_by(name: beer_attributes[:name])
+    beer ||= beer = Beer.create(beer_attributes)
+    self.beer = beer
+  end
+
 end
