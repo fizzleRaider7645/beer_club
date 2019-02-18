b56ed19dee6232fd2125f5c340b6109ae4ca85d0
Douglas Uretsky <douglasuretsky@gmail.com>
2019-02-18 12:09:59 -0500

    update medals index to handle user_id params and display descriptions and points

--- app/controllers/medals_controller.rb
+++ app/controllers/medals_controller.rb
@@ -1,8 +1,20 @@
 class MedalsController < ApplicationController
 
   def index
+    user_index if params.include? :user_id
+    general_index if !params.include? :user_id
+  end
+
+
+  private
+
+  def user_index
     user = User.find_by(id: params[:user_id])
     @medals = user.medals
+  end
+
+  def general_index
+    @medals = Medal.all
   end
 
 end
--- app/helpers/beers_helper.rb
+++ app/helpers/beers_helper.rb
@@ -1,2 +1,7 @@
 module BeersHelper
+  def no_beer_name_error
+    if @review.beer && @review.beer.errors
+      @review.beer.errors.full_messages.first.upcase
+    end
+  end
 end
