72e08931e1e9f92efe8b78ffe2f853131b3a7aae
Douglas Uretsky <douglasuretsky@gmail.com>
2019-02-18 19:05:45 -0500

    update is_current_user method to handle user show page request limiting view to the current users show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :require_login
   skip_before_action :require_login, only: [:home, :login]
   before_action :is_current_user?
-  skip_before_action :is_current_user?, only: [:index, :show, :home, :login, :logout]
+  skip_before_action :is_current_user?, only: [:index, :home, :login, :logout]
 
   private
 
@@ -21,17 +21,19 @@ class ApplicationController < ActionController::Base
   def is_current_user?
     return if !logged_in?
     return if user_account_delete(params)
-    # if !logged_in?
-      # return
-    # else
-      redirect_to '/' unless params[:user_id].to_i == current_user.id
-    # end
+    id = params[:user_id].to_i if params[:user_id]
+    id ||= params[:id].to_i if user_show_page
+    redirect_to '/' unless id == current_user.id
   end
 
   def user_account_delete(params)
     params[:controller] == "users" &&
     params[:action] == "destroy" &&
     params[:id].to_i == current_user.id
+  end
+
+  def user_show_page
+    params["controller"] == "users" && params["action"] == "show"
   end
 
   helper_method :current_user
--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,9 @@ Rails.application.routes.draw do
 
   resources :reviews, only: [:index, :show]
 
-  resources :users do
+  resources :users, only: [:show] do
     resources :medals, only: [:index, :show]
-    resources :beers
+    resources :beers, only: [:show, :index]
     resources :reviews
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
