49653f447c6cd89bcac2980ee3591f73fe6251dd
Douglas Uretsky <douglasuretsky@gmail.com>
2019-03-24 14:34:05 -0400

    update beer show page to load with jQuery

--- app/assets/javascripts/beer.js
+++ app/assets/javascripts/beer.js
@@ -6,12 +6,19 @@ function getBeerReviews(id) {
   $.get(`/beers/${id}/reviews`, function(json) {
     $('#beer-review-box').empty();
     json.data.forEach(function(reviewObj) {
+      $('#beer-review-box').append("<li>" + "<strong>" + reviewObj.attributes.title + "</strong>" + "-" + reviewObj.attributes.text + "</li> <br />")
     });
   });
 }
 
+
 function attachListeners() {
   $('.see-review').on('click', function(e) {
+    let id = this.id
+    e.preventDefault();
+    getBeerReviews(id);
+  });
+  $('.beer-show-page-reviews').on('click', function(e) {
     let id = this.id
     e.preventDefault();
     getBeerReviews(id);
