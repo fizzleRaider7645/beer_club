2beacf36f37f2be3dbc09747a37f8b109c448764
Douglas Uretsky <douglasuretsky@gmail.com>
2019-03-26 13:23:31 -0400

    update postNewReview function to handle nested attributes

--- app/assets/javascripts/beer.js
+++ app/assets/javascripts/beer.js
@@ -31,25 +31,31 @@ function getUserReviews(userId) {
 
 function seeReview(userId, review_id) {
   $.get(`/users/${userId}/reviews/${review_id}` + `.json`, function(json) {
-    let saved = json
     $('.user-reviews-box').empty();
     $('.user-reviews-box').append(`<h2>${json.data.attributes.title}</h2> <p>${json.data.attributes.text}</p> <button onClick=getUserReviews(${json.data.relationships.user.data.id})>Go Back</button>`)
-    $('.user-reviews-box').append(`<button class='edit-review-button'>Edit Review</button>`)
+    $('.user-reviews-box').append(`<button><a style="text-decoration:none" href='/users/${userId}/reviews/${review_id}/edit'>Edit Review</a></button>`)
   });
 }
 
 function postNewReview(userId) {
-  let state = {};
-  $.post(`/users/${userId}/reviews`, function(data) {
-    debugger
+  let reviewDate = $('#review_date').val();
+  let reviewTitle = $('#review_title').val();
+  let reviewRating = $('#review_rating').val();
+  let reviewText = $('#review_text').val();
+  let beerName = $('#review_beer_attributes_name').val();
+  let beerStyle = $('#review_beer_attributes_style').val();
+  let beerBrewery = $('#review_beer_attributes_brewery').val();
+  let beerCountry = $('#review_beer_attributes_country').val();
+  let beerAbv = $('#review_beer_attributes_abv').val();
+  let state = { review: {title: reviewTitle, date: reviewDate, rating: reviewRating, text: reviewText,beer_attributes: {name: beerName, style: beerStyle, country: beerCountry, abv: beerAbv, brewery: beerBrewery}}}
+
+  $.post(`/users/${userId}/reviews`, state, function(data) {
+  }, "json");
+  $.get(`/users/${userId}/reviews` + `.json`, function(json) {
+    let newlyCreatedReview = json.data[json.data.length - 1]
+    $('.review-index-box').append("<li>" + "<strong>" + newlyCreatedReview.attributes.title + "</strong>" + "-" + newlyCreatedReview.relationships.beer.data.name + "</li>")
   });
 }
-
-function editReview() {
-  debugger
-  // $.get()
-}
-
 
 function attachListeners() {
   $('.see-review').on('click', function(e) {
@@ -78,9 +84,6 @@ function attachListeners() {
     let userId = document.getElementsByClassName("review-index-box")[0].id
     e.preventDefault();
     postNewReview(userId);
-  });
-  $('.user-reviews-box').on('click', '.edit-review-button', function(e) {
-    editReview();
   });
 
   $(document).ready(function(e) {
